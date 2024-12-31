1577edf6fb7baf1189ee4c5a2aba5af1958561b3
JUAN_HUN <juanhun2424@gmail.com>
2024-12-31 14:26:36 -0500

    create converter and path for SingleGame View

--- backend/game_app/urls.py
+++ backend/game_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, register_converter
-from .views import AllGames
+from .views import AllGames, SingleGame
+from .converters import IntOrStrConverter
+
+register_converter(IntOrStrConverter, 'int_or_str')
 
 urlpatterns = [
     path('', AllGames.as_view(), name='all_games'),
+    path('<int_or_str:game_identifier>/', SingleGame.as_view(), name='single_game'),
 ]
