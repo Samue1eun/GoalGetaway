20d1ed8222761d39b2412eac1ddb1b464d83f728
samue1eun <samuel.eun.park@gmail.com>
2025-01-03 16:11:22 -0700

    Added in Tailwind and DaisyUI props to the pages

--- frontend/pages/MyFavoriteTeam/MyFavoriteTeam.jsx
+++ frontend/pages/MyFavoriteTeam/MyFavoriteTeam.jsx
@@ -1,8 +1,23 @@
+import TopPlayerStats from '../../components/Cards/Pages/MyFavoriteTeam/TopPlayerStats';
+import CurrentTeamStandings from '../../components/Cards/Pages/MyFavoriteTeam/CurrentTeamStandings';
+import MostRecentScores from '../../components/Cards/Pages/MyFavoriteTeam/MostRecentScores';
+import Schedule from '../../components/Cards/Pages/MyFavoriteTeam/Schedule';
+import TeamNews from '../../components/Cards/Pages/MyFavoriteTeam/TeamNews';
+
 
 const MyFavoriteTeam = () => {
     return (
         <>
             <h1>My Favorite Team</h1>
+            <div className="flex justify-center items-center">
+                <TeamNews />
+            </div>
+            <TopPlayerStats />
+            <CurrentTeamStandings />
+            <MostRecentScores />
+            <Schedule />
+
+
         
         </>
     )
