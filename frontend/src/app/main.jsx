7c11606c6aa2dce5eeab4716cdccbcc6771e8db1
samue1eun <samuel.eun.park@gmail.com>
2025-01-02 23:05:15 -0700

    Deleted AppRouter.jsx file

--- frontend/src/app/main.jsx
+++ frontend/src/app/main.jsx
@@ -3,13 +3,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './index.css';
 
-// const root = createRoot(document.getElementById('root'));
-// root.render(
-//     <BrowserRouter>
-//       <AppRouter />
-//     </BrowserRouter>
-// );
-
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 )
