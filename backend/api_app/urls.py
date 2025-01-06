4675665fef00afeb72c1c8ac3940ed0c5470ec96
JUAN_HUN <juanhun2424@gmail.com>
2025-01-06 12:46:46 -0500

    Add the top ten stats class which allows us to pull the top ten players based on a specific stat of their choosing

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('today_games/', views.GameTodayView.as_view(), name='games-by-date'),
     path('nfl_teams/', views.NFLTeamView.as_view(), name="nfl_teams"),
     path('team_standings/', views.NFLTeamStandings.as_view(), name='team_standings'),
+    path('top_stats/', views.TopTenStats.as_view(), name='top_ten_stats_request'),
 ]
