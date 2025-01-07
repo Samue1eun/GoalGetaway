f54f956c6eefdd311e417d2fb3cdfe52f4cc2934
samue1eun <samuel.eun.park@gmail.com>
2025-01-06 23:34:37 -0700

    Added favorite teams list from third party API

--- frontend/components/Settings/FavoriteTeamSelection/FavoriteTeamSelection.jsx
+++ frontend/components/Settings/FavoriteTeamSelection/FavoriteTeamSelection.jsx
@@ -1,9 +1,34 @@
-
+import { use } from 'react';
+import { useState, useEffect } from 'react';
 
 const FavoriteTeamSelection = () => {
+    const [teams, setTeams] = useState([]);
+    const [selectedTeam, setSelectedTeam] = useState('');
+
+    useEffect(() => {
+        // Fetch the JSON data from the API
+        fetch('http://127.0.0.1:8000/api/v1/api_app/nfl_teams/')
+            .then(response => response.json())
+            .then(data => setTeams(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+
+    const handleChange = (event) => {
+        setSelectedTeam(event.target.value);
+    };
+    
     return (
         <>
-        
+            <div className="dropdown dropdown-right">
+                <div tabIndex={0} role="button" className="btn m-1">Team Selection</div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    {teams.map(team => (
+                        <li key={team.TeamID}>
+                            <a onClick={() => setSelectedTeam(team.FullName)}>{team.FullName}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>  
         </>
     )
 }
--- frontend/pages/Settings/Settings.jsx
+++ frontend/pages/Settings/Settings.jsx
@@ -1,4 +1,6 @@
 import SettingsNavBar from "../../components/NavBar/Settings/SettingsNavBar";
+import FavoriteTeamSelection from "../../components/Settings/FavoriteTeamSelection/FavoriteTeamSelection";
+
 
 const Settings = () => {
     return (
@@ -7,6 +9,11 @@ const Settings = () => {
                 <SettingsNavBar />
             </div>
             <h1 className="text-center text-3xl font-bold my-4">Settings</h1>
+            <div className="flex flex-col items-center">
+                <div className="w-full p-4 flex justify-center">
+                    <FavoriteTeamSelection />
+                </div>
+            </div>
         </>
     )
 }
