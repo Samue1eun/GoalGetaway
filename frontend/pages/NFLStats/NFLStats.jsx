3c33aa9509edfbe1e3021f467cf0568be1d0a9f1
samue1eun <samuel.eun.park@gmail.com>
2025-01-08 08:53:41 -0700

    Adding cards to the NFL Stats Page

--- frontend/pages/NFLStats/NFLStats.jsx
+++ frontend/pages/NFLStats/NFLStats.jsx
@@ -1,8 +1,11 @@
 import NFLStatsNavBar from "../../components/NavBar/NFL Stats/NFLStatsNavBar";
-import TopLeaguePlayers from "../../components/Cards/Pages/NFLStatistics/TopLeaguePlayers";
-import TopOffensivePlayers from "../../components/Cards/Pages/NFLStatistics/TopOffensivePlayers";
-import TopDefensivePlayers from "../../components/Cards/Pages/NFLStatistics/TopDefensivePlayers";
-
+// import TopLeaguePlayers from "../../components/Cards/Pages/NFLStatistics/TopLeaguePlayers";
+// import TopOffensivePlayers from "../../components/Cards/Pages/NFLStatistics/TopOffensivePlayers";
+// import TopDefensivePlayers from "../../components/Cards/Pages/NFLStatistics/TopDefensivePlayers";
+import KickerPercentageCompletion from "../../components/Cards/Pages/NFLStatistics/Offense/KickerPercentageCompletion";
+import TopPassingYards from "../../components/Cards/Pages/NFLStatistics/Offense/TopPassingYards";
+import TopReceivingYards from "../../components/Cards/Pages/NFLStatistics/Offense/TopReceivingYards";
+import TopRushingYards from "../../components/Cards/Pages/NFLStatistics/Offense/TopRushingYards";
 
 const NFLStats = () => {
     return (
@@ -11,15 +14,19 @@ const NFLStats = () => {
                 <NFLStatsNavBar />
             </div>
             <h1 className="text-center text-3xl font-bold my-4">NFL Statistics</h1>
-            <div className="flex flex-col items-center">
-                <div className="w-full p-4 flex justify-center">
-                    <TopLeaguePlayers />
+            <div className="flex flex-col items-start">
+                <h2 className="text-center text-3xl font-bold my-4">Offense</h2>
+                <div className="w-full md:w-3/4 p-4 flex justify-start">
+                    <TopPassingYards />
                 </div>
-                <div className="w-full p-4 flex justify-center">
-                    <TopOffensivePlayers />
+                <div className="w-full md:w-3/4 p-4 flex justify-start">
+                    <TopRushingYards />
                 </div>
-                <div className="w-full p-4 flex justify-center">
-                    <TopDefensivePlayers />
+                <div className="w-full md:w-3/4 p-4 flex justify-start">
+                    <TopReceivingYards />
+                </div>
+                <div className="w-full md:w-3/4 p-4 flex justify-start">
+                    <KickerPercentageCompletion />
                 </div>
             </div>
         </>
