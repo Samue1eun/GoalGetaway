a00fbd59f4340dad2f562b30f298afa8d69cc0c7
JUAN_HUN <juanhun2424@gmail.com>
2025-01-15 16:53:54 -0500

    add upcoming trips to homepaged and routed to the back

--- backend/trips_app/models.py
+++ backend/trips_app/models.py
@@ -3,6 +3,6 @@ from user_app.models import User
 # Create your models here.
 class Trips(models.Model):
   name = models.CharField(max_length=255, blank=False)
-  image_url = models.CharField(max_length=255, blank=False)
+  image_url = models.TextField( blank=False)
   price = models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
   user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trips')
